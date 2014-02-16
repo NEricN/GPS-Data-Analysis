89c93ee40f893f2857eae388be861a1d3ba6808b
joshzarrabi <joshzarrabi@berkeley.edu>
2014-02-16 01:43:06 -0800

    Fixed Bugs

--- Parser.java
+++ Parser.java
@@ -1,20 +1,26 @@
 import java.io.File;
 import java.util.Scanner;
 
+import java.io.FileNotFoundException;
+
 public class Parser {
 
-	public static void parseFile (String filename, Graph myGraph) {
+	private Graph myGraph;
+
+	public static void parseFile (String filename, Graph myGraph) throws FileNotFoundException {
 		File file = new File(filename);
 		double lat, longi;
 		String pic;
 		Scanner inputStream = new Scanner(file);
 		inputStream.useDelimiter(",");
 		while (inputStream.hasNext()) {
-			lat = inputStream.next();
-			longi = inputStream.next();
+			lat = Double.parseDouble(inputStream.next());
+			longi = Double.parseDouble(inputStream.next());
 			pic = inputStream.next();
 			Node myNode = new Node(lat, longi, pic);
 			myGraph.addNode(myNode);
 		}
 	}
+
+
 }
